cfc37ce6ec8c7c5af7589f0fb5490e0f856f7319
Aishani-Roy <aishaniroy2202@gmail.com>
2026-05-09 13:48:08 +0530

    py

---
+++ reverse.py
@@ -0,0 +1,5 @@
+message=input("enter your message")
+reverse=""
+for charecter in message:
+    reverse=charecter+reverse
+print("reverse of message is",reverse)    
---
+++ sum.py
@@ -0,0 +1,5 @@
+n=int(input("enter number:"))
+sum=0
+for i in range(1,n+1,1):
+    sum=sum+i
+print("the sum of first",n,"natural numbers is",sum)    
